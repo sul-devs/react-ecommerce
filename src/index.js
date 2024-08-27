9d29e9da36de6e94c5555aa2dd02a48fa60abd5e
Sul <sulaimankamara110@gmail.com>
2024-08-27 19:23:24 +0100

    Create ProductPageLink component.

--- src/index.js
+++ src/index.js
@@ -33,6 +33,20 @@ const Button = ({ buttonText }) => {
   return <button className="button product-card-button">{buttonText}</button>;
 };
 
+// productPageLink
+const ProductPageLink = ({
+  linkText = "Buy Now",
+  category,
+  id,
+  productName,
+}) => {
+  return (
+    <a href={category / id} title={`Go to ${productName} page.`}>
+      {linkText}
+    </a>
+  );
+};
+
 // Render app
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Button buttonText="I'm a button :)" />);
+root.render(<ProductPageLink category="Devices" id="1" productName="Laptop" />);
