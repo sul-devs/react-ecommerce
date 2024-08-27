51df9a3e22f59e23cc2df786c45533b1b75bd920
Sul <sulaimankamara110@gmail.com>
2024-08-27 18:38:02 +0100

    Create ProductCardImage component.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 // Imports
 import React from "react";
 import ReactDOM from "react-dom/client";
+import laptop from "./assets/images/laptop.jpg";
+
+// ProductCardImage
+const ProductCardImage = ({ src, alt, width }) => {
+  return <img src={src} alt={alt} width={250} height={200} />;
+};
 
 // Render app
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render();
+root.render(<ProductCardImage src={laptop} alt="Laptop" />);
