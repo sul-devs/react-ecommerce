96f7d62ff68c618bb6b5af98b458277bf7534312
Sul <sulaimankamara110@gmail.com>
2024-08-27 18:46:33 +0100

    Create ProductCardText component

--- src/index.js
+++ src/index.js
@@ -2,12 +2,38 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import laptop from "./assets/images/laptop.jpg";
+import "./app.css";
 
 // ProductCardImage
 const ProductCardImage = ({ src, alt, width }) => {
-  return <img src={src} alt={alt} width={250} height={200} />;
+  return (
+    <img
+      className="product-card-image"
+      src={src}
+      alt={alt}
+      width={250}
+      height={200}
+    />
+  );
+};
+
+// ProductCardText
+const ProductCardText = ({ productName, productSummary, productPrice }) => {
+  return (
+    <>
+      <h3>{productName}</h3>
+      <p>{productSummary}</p>
+      <p>{`£${productPrice}`}</p>
+    </>
+  );
 };
 
 // Render app
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ProductCardImage src={laptop} alt="Laptop" />);
+root.render(
+  <ProductCardText
+    productName="Laptop"
+    productSummary="This is a laptop."
+    productPrice={1999.99}
+  />
+);
