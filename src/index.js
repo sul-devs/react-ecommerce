d8e918de7feb6ee77fa9131183e458a29448e06a
Sul <sulaimankamara110@gmail.com>
2024-08-31 22:08:41 +0100

    Render products using map.

--- src/index.js
+++ src/index.js
@@ -1,173 +1,89 @@
 // Imports
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./app.css";
-import laptop from "./assets/images/laptop.jpg";
-import airpods from "./assets/images/airpods.jpg";
-import camera from "./assets/images/camera.jpg";
-import drone from "./assets/images/drone.jpg";
-import phone from "./assets/images/phone.jpg";
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./app.css"
+import laptop from "./assets/images/laptop.jpg"
+import airpods from "./assets/images/airpods.jpg"
+import camera from "./assets/images/camera.jpg"
+import drone from "./assets/images/drone.jpg"
+import phone from "./assets/images/phone.jpg"
 
-// Product objects (data)
-
-// Product 1 Data
-const productOne = {
-  src: laptop,
-  alt: "Laptop",
-  productName: "Maqbook Pro",
-  productSummary: "Quick, efficient laptop for the workplace.",
-  productPrice: 249.99,
-  category: "Devices",
-  id: 1,
-};
-const {
-  src: srcOne,
-  alt: altOne,
-  productName: productNameOne,
-  productSummary: productSummaryOne,
-  productPrice: productPriceOne,
-  category: categoryOne,
-  id: idOne,
-} = productOne;
-
-// Product 2 Data
-const productTwo = {
-  src: airpods,
-  alt: "Airpoods Pro",
-  productName: "Earpods Pro",
-  productSummary: "Comfy earphones, with great sound quality.",
-  productPrice: 99.99,
-  category: "Devices",
-  id: 2,
-};
-const {
-  src: srcTwo,
-  alt: altTwo,
-  productName: productNameTwo,
-  productSummary: productSummaryTwo,
-  productPrice: productPriceTwo,
-  category: categoryTwo,
-  id: idTwo,
-} = productTwo;
-
-// Product 3 Data
-const productThree = {
-  src: camera,
-  alt: "Camera",
-  productName: "Camera NMPO21",
-  productSummary: "High quality, antique camera.",
-  productPrice: 1199.99,
-  category: "Devices",
-  id: 3,
-};
-const {
-  src: srcThree,
-  alt: altThree,
-  productName: productNameThree,
-  productSummary: productSummaryThree,
-  productPrice: productPriceThree,
-  category: categoryThree,
-  id: idThree,
-} = productThree;
-
-// Product 4 Data
-const productFour = {
-  src: drone,
-  alt: "Drone",
-  productName: "Drone MQ893",
-  productSummary: "Drone with 4K HD camera built-in.",
-  productPrice: 2499.99,
-  category: "Devices",
-  id: 4,
-};
-const {
-  src: srcFour,
-  alt: altFour,
-  productName: productNameFour,
-  productSummary: productSummaryFour,
-  productPrice: productPriceFour,
-  category: categoryFour,
-  id: idFour,
-} = productFour;
-
-// Product 5 Data
-const productFive = {
-  src: phone,
-  alt: "Lphone 12 Max",
-  productName: "Lphone 12 Pro",
-  productSummary: "Durable, lightweight smart phone.",
-  productPrice: 899.99,
-  category: "Devices",
-  id: 5,
-};
-const {
-  src: srcFive,
-  alt: altFive,
-  productName: productNameFive,
-  productSummary: productSummaryFive,
-  productPrice: productPriceFive,
-  category: categoryFive,
-  id: idFive,
-} = productFive;
+// Products data
+const products = [
+  {
+    src: laptop,
+    alt: "Laptop",
+    productName: "Maqbook Pro",
+    productSummary: "Quick, efficient laptop for the workplace.",
+    productPrice: 249.99,
+    category: "Devices",
+    id: 1,
+  },
+  {
+    src: airpods,
+    alt: "Airpoods Pro",
+    productName: "Earpods Pro",
+    productSummary: "Comfy earphones, with great sound quality.",
+    productPrice: 99.99,
+    category: "Devices",
+    id: 2,
+  },
+  {
+    src: camera,
+    alt: "Camera",
+    productName: "Camera NMPO21",
+    productSummary: "High quality, antique camera.",
+    productPrice: 1199.99,
+    category: "Devices",
+    id: 3,
+  },
+  {
+    src: drone,
+    alt: "Drone",
+    productName: "Drone MQ893",
+    productSummary: "Drone with 4K HD camera built-in.",
+    productPrice: 2499.99,
+    category: "Devices",
+    id: 4,
+  },
+  {
+    src: phone,
+    alt: "Lphone 12 Max",
+    productName: "Lphone 12 Pro",
+    productSummary: "Durable, lightweight smart phone.",
+    productPrice: 899.99,
+    category: "Devices",
+    id: 5,
+  },
+]
 
 // ProductList
 const ProductList = (props) => {
-  const { listName, sectionHeading } = props;
+  const { listName, sectionHeading } = props
   return (
     <section id={listName}>
       <div className="container">
         <h2>{sectionHeading}</h2>
         <ul className="product-list featured-list laptops-list">
-          <ProductCard
-            src={srcOne}
-            alt={altOne}
-            productName={productNameOne}
-            productSummary={productSummaryOne}
-            productPrice={productPriceOne}
-            category={categoryOne}
-            id={idOne}
-          />
-          <ProductCard
-            src={srcTwo}
-            alt={altTwo}
-            productName={productNameTwo}
-            productSummary={productSummaryTwo}
-            productPrice={productPriceTwo}
-            category={categoryTwo}
-            id={idTwo}
-          />
-          <ProductCard
-            src={srcThree}
-            alt={altThree}
-            productName={productNameThree}
-            productSummary={productSummaryThree}
-            productPrice={productPriceThree}
-            category={categoryThree}
-            id={idThree}
-          />
-          <ProductCard
-            src={srcFour}
-            alt={altFour}
-            productName={productNameFour}
-            productSummary={productSummaryFour}
-            productPrice={productPriceFour}
-            category={categoryFour}
-            id={idFour}
-          />
-          <ProductCard
-            src={srcFive}
-            alt={altFive}
-            productName={productNameFive}
-            productSummary={productSummaryFive}
-            productPrice={productPriceFive}
-            category={categoryFive}
-            id={idFive}
-          />
+          {products.map((product) => {
+            return (
+              <ProductCard
+                src={product.src}
+                alt={product.alt}
+                width={product.width}
+                productName={product.productName}
+                productSummary={product.productSummary}
+                productPrice={product.productPrice}
+                category={product.category}
+                id={product.id}
+              />
+            )
+          })}
         </ul>
       </div>
     </section>
-  );
-};
+  )
+}
 
 // ProductCard
 const ProductCard = (props) => {
@@ -180,7 +96,7 @@ const ProductCard = (props) => {
     productPrice,
     category,
     id,
-  } = props;
+  } = props
   return (
     <li>
       <article className="product-card">
@@ -201,17 +117,16 @@ const ProductCard = (props) => {
         <AddToWishListButton productName={productName} />
       </article>
     </li>
-  );
-};
+  )
+}
 
 ProductCard.defaultProps = {
   width: 230,
-};
+}
 
 // ImageLink
 const ImageLink = (props) => {
-  const { src, alt, width, height, productName, category, id } = props;
-
+  const { src, alt, width, height, productName, category, id } = props
   return (
     <a href={`${category}/${id}`} title={`Go to ${productName} page.`}>
       <Image
@@ -222,13 +137,12 @@ const ImageLink = (props) => {
         height={height}
       />
     </a>
-  );
-};
+  )
+}
 
 // AddToCartButton
 const AddToCartButton = (props) => {
-  const { productName } = props;
-
+  const { productName } = props
   return (
     <button
       className="button secondary-button product-card-button"
@@ -236,13 +150,12 @@ const AddToCartButton = (props) => {
     >
       Add To Cart 🛒
     </button>
-  );
-};
+  )
+}
 
 // AddToWishListButton
 const AddToWishListButton = (props) => {
-  const { productName } = props;
-
+  const { productName } = props
   return (
     <button
       className="button secondary-button product-card-button"
@@ -250,12 +163,12 @@ const AddToWishListButton = (props) => {
     >
       Add To WishList 🎁
     </button>
-  );
-};
+  )
+}
 
 // BuyNowButton
 const BuyNowButton = (props) => {
-  const { productName, category, id } = props;
+  const { productName, category, id } = props
   return (
     <a
       className="button primary-button product-card-button"
@@ -264,20 +177,20 @@ const BuyNowButton = (props) => {
     >
       Buy Now ➡
     </a>
-  );
-};
+  )
+}
 
 // Image
 const Image = (props) => {
-  const { src, alt, width } = props;
-  return <img className="image" src={src} alt={alt} width={width} />;
-};
+  const { src, alt, width } = props
+  return <img className="image" src={src} alt={alt} width={width} />
+}
 
 // ProductRating
 const ProductRating = () => {
-  return <p>XXXX (4)</p>;
-};
+  return <p>XXXX (4)</p>
+}
 
 // Render app
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<ProductList listName="featured" sectionHeading="Featured" />);
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<ProductList listName="featured" sectionHeading="Featured" />)
